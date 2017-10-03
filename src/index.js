f9d6f4d7afe5c28b4e1fc8b7f2218cccdbb8c91a
mazarady <munirm@uci.edu>
2017-10-02 21:01:02 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// First Component 
+const App = () => {
+
+	return (
+		<div>
+		<h1>Hello Maaz Munir</h1>
+		<p> I am an Informatics Student </p>
+		</div>
+
+
+		)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
